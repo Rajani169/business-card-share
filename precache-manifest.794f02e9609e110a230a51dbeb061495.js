bf177eecce7d89e3f36e609a66d3afeea662c33e
Rajani169 <sharmarajani169@gmail.com>
2021-04-23 00:28:56 +0530

    Updates

---
+++ precache-manifest.794f02e9609e110a230a51dbeb061495.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eaed5dc2a099c10da5bba6c1703ff44",
+    "url": "/business-card-share/index.html"
+  },
+  {
+    "revision": "45fe9dc9da5c068f3de9",
+    "url": "/business-card-share/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9caf1c7be8eeb31510e0",
+    "url": "/business-card-share/static/css/main.b3e53094.chunk.css"
+  },
+  {
+    "revision": "45fe9dc9da5c068f3de9",
+    "url": "/business-card-share/static/js/2.0c14b53d.chunk.js"
+  },
+  {
+    "revision": "9caf1c7be8eeb31510e0",
+    "url": "/business-card-share/static/js/main.1d9a0730.chunk.js"
+  },
+  {
+    "revision": "8ebc89a164516507da8a",
+    "url": "/business-card-share/static/js/runtime~main.9f09a302.js"
+  },
+  {
+    "revision": "86f1c61b55277a6d4857dbb7a2b06de9",
+    "url": "/business-card-share/static/media/4.86f1c61b.png"
+  },
+  {
+    "revision": "62baa0b992c9037ee7f1c4e41c0dff8a",
+    "url": "/business-card-share/static/media/app.62baa0b9.svg"
+  },
+  {
+    "revision": "2aee737878818d0ad5453d604fa5a81d",
+    "url": "/business-card-share/static/media/avtar.2aee7378.png"
+  },
+  {
+    "revision": "d9b52cb8597e175f63f9f9ab6009b9df",
+    "url": "/business-card-share/static/media/banner.d9b52cb8.jpg"
+  },
+  {
+    "revision": "ff82149f7454b9ecb83a867a7168e538",
+    "url": "/business-card-share/static/media/card1.ff82149f.png"
+  },
+  {
+    "revision": "7bee897890efb6a8f63fb24e8e0fde62",
+    "url": "/business-card-share/static/media/card2.7bee8978.png"
+  },
+  {
+    "revision": "c797607e9ad360e23d282c9206f7481f",
+    "url": "/business-card-share/static/media/card3.c797607e.png"
+  },
+  {
+    "revision": "b48d47f86341b5fdb7536ca279a50b99",
+    "url": "/business-card-share/static/media/card4.b48d47f8.png"
+  },
+  {
+    "revision": "d20050914a1fa8af41095b45aff9a0dc",
+    "url": "/business-card-share/static/media/card5.d2005091.png"
+  },
+  {
+    "revision": "e5f7b561bbb0b70dbaf74de2c9ab4af1",
+    "url": "/business-card-share/static/media/login.e5f7b561.png"
+  },
+  {
+    "revision": "7f5683fb13ab2c71bef7d4b18f6c3905",
+    "url": "/business-card-share/static/media/men-img.7f5683fb.png"
+  },
+  {
+    "revision": "ff531371b11792d003f420fc3697a6f5",
+    "url": "/business-card-share/static/media/signup.ff531371.png"
+  }
+]);
